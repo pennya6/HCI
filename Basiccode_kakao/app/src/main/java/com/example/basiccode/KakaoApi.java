966d37d76c4390cb988e0de7157bef9f06c2ebbc
kimsiyeon <pennya6@hs.ac.kr>
2022-05-10 00:55:25 +0900

    수정중

---
+++ Basiccode_kakao/app/src/main/java/com/example/basiccode/KakaoApi.java
@@ -0,0 +1,11 @@
+package com.example.basiccode;
+
+import android.content.ContentValues;
+
+import java.net.HttpURLConnection;
+
+public class KakaoApi {
+    String API_URL="https://dapi.kakao.com/v2/vision/multitage/generate";
+    String MYAPP_KEY="342ee2207ae205492513ec8442f89419";
+    
+}
